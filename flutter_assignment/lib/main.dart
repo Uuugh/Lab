feca9bfda1bdaead439d1d4b50285af6499ac8a8
Richard Friendlich <Rich Friendlich>
2020-05-28 05:13:56 -0400

    Added in main.. where is the rest of lib?

---
+++ flutter_assignment/lib/main.dart
@@ -0,0 +1,48 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+import 'package:flutter/material.dart';
+
+import './text.dart';
+import './textControl.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _textString = "Hi there good looking!";
+
+  void _changeText(String textString) {
+    setState(() {
+      this._textString = textString;
+    });
+    print('_changeText');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Assignment 1'),
+        ),
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            children: [
+              PrintText(_textString),
+              TextControl(_changeText,"What are you looking at?"),
+            ],
+            mainAxisSize: MainAxisSize.max,
+          ),
+        ));
+
+    return MaterialApp(
+      home: scaffold,
+    );
+  }
+}
